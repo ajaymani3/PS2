f74c8af9e47b3190704dce2272f2a3affd856a4e
Ajay <ajaymani@udel.edu>
2016-02-26 22:48:20 -0500

    integer set

---
+++ PS2/src/main/java/base/MyInteger.java
@@ -0,0 +1,95 @@
+package base;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+
+import org.junit.AfterClass;
+
+import org.junit.Before;
+
+import org.junit.BeforeClass;
+
+import org.junit.Test;
+
+
+public class MyInteger {
+	
+	public int iValue;
+	
+	public MyInteger(int iValue){ 
+		 
+	}
+	
+	public static void main(String[] args){
+	
+
+	}
+
+	
+	
+	public boolean isEven(int a){
+		boolean even;
+		even = (a % 2 == 0);
+		return even;			
+		
+	}
+	
+	public boolean isOdd(int a){
+		boolean odd;
+		odd = (a % 2 == 1);
+		return odd;
+	
+	}
+	
+	public boolean isPrime(int a){
+		
+		boolean prime;
+		prime = (a % 2 == 0);
+		return prime;
+		
+	}
+	
+	public static boolean isEven(){
+		boolean (a % 2 == 0);
+		return true;
+		
+	}
+	
+	public static int isOdd(int){
+		even = (a % 2 == 1);
+		return true;
+		
+	}
+	
+	public static int isPrime(int){
+		even = (a % 2 == 0);
+		return true;
+		
+	}
+	
+	public static int isEven(MyInteger){
+		MyInteger = (a % 2 == 0);
+		return true;
+	}
+	
+	public static int isOddMyInteger){
+		even = (a % 2 == 1);
+		return true;
+	}
+	
+	public static int isPrime(MyInteger){
+		even = (a % 2 == 0);
+		return true;
+	}
+	
+	public equals(int){
+		return true;
+		
+	}
+	
+	public equals(MyOnteger){
+		return true;
+		
+	}
+}
